1b79cf76b1f4cf80b670a433e65cf29a2127f300
Heer Patel <108482463+HeerPatel30@users.noreply.github.com>
2025-10-05 15:05:59 +0530

    vercel cors fix

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin:[ 'http://localhost:5173','https://ai-grammer-app.vercel.app/'] ,// allow your frontend origin
+  origin:[ 'http://localhost:5173','https://ai-grammer-app.vercel.app'] ,// allow your frontend origin
   credentials: true
 }));
 app.get('/', (req, res) => {
