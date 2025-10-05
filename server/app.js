99ec1527f572529e0b2896d44a59f8a5d5966f25
Heer Patel <108482463+HeerPatel30@users.noreply.github.com>
2025-10-05 14:31:07 +0530

    cors policy for local updated

--- server/app.js
+++ server/app.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: '*' }));
+app.use(cors({
+  origin: 'http://localhost:5173', // allow your frontend origin
+  credentials: true
+}));
 app.get('/', (req, res) => {
   res.json({corrected:"" , originalText:""})
 })
