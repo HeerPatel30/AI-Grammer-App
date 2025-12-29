c3a6a2e01274f5b9e1c1454bcc0982f7daa2ed8d
Heer Patel <108482463+HeerPatel30@users.noreply.github.com>
2025-12-29 15:15:01 +0530

    Switch email sending from nodemailer to Brevo
    Replaced nodemailer configuration with Brevo API for sending emails.

--- server/Config/Config.js
+++ server/Config/Config.js
@@ -1,15 +1,39 @@
 import nodemailer from "nodemailer";
 import { client } from "../Redis/redisconnect.js";
+import brevo from "@getbrevo/brevo"
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail", // or use your SMTP config
+//   auth: {
+//     user: "heerpatel879@gmail.com",
+//     pass: "gxza uzma buwk fsxm" // use App Password if Gmail
+//   }
+// });
 
 
-const transporter = nodemailer.createTransport({
-  service: "gmail", // or use your SMTP config
-  auth: {
-    user: "heerpatel879@gmail.com",
-    pass: "gxza uzma buwk fsxm" // use App Password if Gmail
+let apiInstance = new brevo.TransactionalEmailsApi();
+apiInstance.setApiKey( 
+  brevo.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.BREVO_API_KEY
+);
+
+async function sendemail(to, subject, text) {
+  try {
+    let sendSmtpEmail = new brevo.SendSmtpEmail();
+    sendSmtpEmail = {
+      to: [{ email: to }],
+      sender: { email: "heerpatel879@gmail.com" },
+      subject: subject,
+      textContent: text,
+    };
+    const data = apiInstance.sendTransacEmail(sendSmtpEmail);
+   
+    return { success: true, messageId: data.messageId };
+  } catch (error) {
+   
+    return { success: false, message: error.message };
   }
-});
-
+}
 
 
 function generatesixdigitsid(){
